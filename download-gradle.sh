3fb55c5901775ccbfb116574020d04dc460d1e82
kunluna <kunluna@users.noreply.github.com>
2026-07-07 18:32:28 +0800

    chore: remove non-code files (docs, scripts, backups) and update .gitignore

--- download-gradle.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/openclaw/.openclaw/workspace/yishell
-mkdir -p ~/.gradle/wrapper/dists/gradle-9.6.0-bin/temp
-curl -L -o ~/.gradle/wrapper/dists/gradle-9.6.0-bin/temp/gradle-9.6.0-bin.zip "https://downloads.gradle.org/distributions/gradle-9.6.0-bin.zip"
